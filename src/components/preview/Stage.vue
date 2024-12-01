0c9a7d6ee71fde9830532792830f03392a7dbfb6
HI_OuO <zvn2060@gmail.com>
2024-12-01 17:30:21 +0800

    chore: 調整 Stage layout

--- src/components/preview/Stage.vue
+++ src/components/preview/Stage.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import {TransitionFade} from "@morev/vue-transitions";
-import {isNil} from "lodash-es";
-import type {Team} from "../../libs";
+import { isNil } from "lodash-es";
+import type { Team } from "../../libs";
 
 const props = defineProps<{ team: Omit<Team, "">; stageId: number }>();
 const stage = computed(() => props.team.stages[props.stageId]);
@@ -13,14 +12,14 @@ function getBorderColor(targetId: StudentId | null | undefined) {
   return getColor(props.team.skillTargetMap.get(targetId) ?? -1);
 }
 
-const container = ref();
+const container = useTemplateRef("container");
 const hover = useElementHover(container);
 </script>
 
 <template>
-  <div ref="container" class="w-fit stage-container">
+  <div ref="container" class="stage-container" :class="{'bg-yellow-50': hover}">
     <Inplace
-        :pt="{
+      :pt="{
         content: { class: ['!flex', 'items-center'] },
         display: { class: ['!border-none'] },
       }"
@@ -29,62 +28,54 @@ const hover = useElementHover(container);
         <div class="stage-description">{{ stage.comment || "&nbsp;" }}</div>
       </template>
       <template #content="{ closeCallback }">
-        <InputText v-model="stage.comment" class="flex-1"/>
+        <InputText v-model="stage.comment" class="flex-1" />
         <Button
-            size="small"
-            class="ml-1"
-            icon="pi pi-check"
-            text
-            rounded
-            @click="closeCallback"
+          size="small"
+          class="ml-1"
+          icon="pi pi-check"
+          text
+          rounded
+          @click="closeCallback"
         />
       </template>
     </Inplace>
     <div class="stage-avatars-container">
       <template v-for="action in stage.actions">
         <StudentAvatar
-            :student="team.getMember(action.actor)"
-            class="border-4 w-[150px]"
-            :style="{ 'border-color': getBorderColor(action.target) }"
+          :student="team.getMember(action.actor)"
+          class="border-4 w-[150px]"
+          :style="{ 'border-color': getBorderColor(action.target) }"
         />
       </template>
     </div>
-    <TransitionFade no-move group class="stage-control">
-      <div v-if="hover" class="">
-        <i class="pi pi-arrow-up" @click="team.moveStage(stageId, 'front')"/>
-        <i class="pi pi-arrow-down" @click="team.moveStage(stageId, 'back')"/>
-        <MdiTableRowPlusBefore
-            class="pi pi-plus text-green-600"
-            @click="team.insertStage(stageId, 'front')"
-        />
-        <MdiTableRowPlusAfter
-            class="pi pi-plus text-green-600 "
-            @click="team.insertStage(stageId, 'back')"
-        />
-        <i
-            class="pi pi-trash text-red-600"
-            @click="team.deleteStage(stageId)"
-        />
-      </div>
-    </TransitionFade>
+    <div class="stage-control" :class="[hover ? 'opacity-100' : 'opacity-0']">
+      <i class="pi pi-arrow-up" @click="team.moveStage(stageId, 'front')" />
+      <i class="pi pi-arrow-down" @click="team.moveStage(stageId, 'back')" />
+      <MdiTableRowPlusBefore
+        class="pi pi-plus text-green-600"
+        @click="team.insertStage(stageId, 'front')"
+      />
+      <MdiTableRowPlusAfter
+        class="pi pi-plus text-green-600"
+        @click="team.insertStage(stageId, 'back')"
+      />
+      <i class="pi pi-trash text-red-600" @click="team.deleteStage(stageId)" />
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 .stage-container {
-  @apply relative flex flex-col gap-[20px];
+  @apply relative flex flex-col gap-[20px] p-4 rounded-md;
   .stage-avatars-container {
     @apply flex gap-[10px] flex-1 max-w-[630px] flex-wrap;
   }
 }
 
 .stage-control {
-  @apply absolute left-full top-0 bottom-0 pl-3 -my-4 -mr-4 py-4;
-  > div {
-    @apply flex items-center gap-2 bg-surface-100 px-2 py-1 rounded-lg;
-    > * {
-      @apply text-2xl cursor-pointer hover:bg-surface-200 w-10 h-10 p-2  rounded leading-none;
-    }
+  @apply transition-opacity flex items-center gap-2 bg-surface-100 px-2 py-1 rounded-lg w-fit;
+  > * {
+    @apply text-2xl cursor-pointer hover:bg-surface-200 w-10 h-10 p-2  rounded leading-none;
   }
 }
 
